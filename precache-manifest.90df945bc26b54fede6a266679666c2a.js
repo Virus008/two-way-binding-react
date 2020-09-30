ee7318a442995045209a91a75f424a735a7fbeb8
virus008 <sonavalevrushabh@gmail.com>
2020-09-30 23:17:52 +0530

    Updates

---
+++ precache-manifest.90df945bc26b54fede6a266679666c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb530ea48327397bea258d1ccc94940",
+    "url": "/two-way-binding-react/index.html"
+  },
+  {
+    "revision": "a36fd9da4fde04248377",
+    "url": "/two-way-binding-react/static/css/main.7c51b0a1.chunk.css"
+  },
+  {
+    "revision": "a9b5c32f0be8eb1414c9",
+    "url": "/two-way-binding-react/static/js/2.994555d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/two-way-binding-react/static/js/2.994555d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36fd9da4fde04248377",
+    "url": "/two-way-binding-react/static/js/main.098fc5a1.chunk.js"
+  },
+  {
+    "revision": "4609a057efb041b0394f",
+    "url": "/two-way-binding-react/static/js/runtime-main.eb1fa4f1.js"
+  }
+]);
